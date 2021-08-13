9d9e78196328d6ac94e9c90dafb4c3c135c923f4
Billy⚡️ <will.js@protonmail.com>
2021-08-13 18:05:56 -0500

    Add files via upload

---
+++ toyShopAutoBuy.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name        Toy Shop
+// @namespace   Neopets Auto Buy
+// @match       https://www.neopets.com/objects.phtml?obj_type=3&type=shop
+// @match       https://www.neopets.com/objects.phtml?obj_type=3&type=shop
+// @require     https://raw.githubusercontent.com/themagicteeth/autobuy/main/autoBuy.js
+// @require     https://raw.githubusercontent.com/themagicteeth/autobuy/main/autoHaggle.js
+// @grant       none
+// @version     1.0
+// @author      -
+// @description Auto buy for Toy Shop
+// ==/UserScript==
+
+const itemsToBuy = [
+    "Usukicon Y22 Goodie Bag",
+    "Neopets 21st Birthday Goodie Bag",
+    "Halloween Y14 Goodie Bag",
+    "Elephante Lamp Collectable Charm",
+    "Sloth Collectable Charm",
+    "Techo Statue Collectable Charm",
+    "Gallion Collectable Charm",
+    "Taiko Standing Drum Collectable Charm",
+    "Princess Terrana Collectable Charm",
+    "Wherfy Collectable Charm",
+    "Spite Doll",
+    "Malice Doll",
+    "Vanity Doll",
+    "Space Faerie Doll",
+    "Soup Faerie Doll",
+    "Water Faerie Doll",
+    "Fire Faerie Doll",
+    "Negg Faerie Doll",
+    "Tooth Faerie Doll",
+    "Air Faerie Doll",
+    "Dark Faerie Doll",
+    "Light Faerie Doll",
+    "A Grey Faerie Doll",
+    "Illusen Faerie Doll",
+    "Taelia Quiguki",
+    "Lost Desert Quiguki",
+    "Year 9 Quiguki",
+    "Jeran Quiguki",
+    "Quiguki Mermaid",
+    "Blushing Bride Quiguki Doll",
+    "King Skarl Quiguki",
+    "Hannah Quiguki",
+    "Beautiful Hair Quiguki Doll",
+    "Luxury Faerie Festival Snowglobe",
+    "Luxury Beach Scene Faerie Snowglobe",
+    "Luxury Dark and Light Faerie Snowglobe",
+    "Luxury Dark and Earth Faerie Snowglobe",
+    "Negg Faerie Snowglobe",
+    "Cyodrake Collectable Charm",
+    "Baby Techo Bobblehead",
+    "Floud Balloon",
+    "Glowing Mushrooms"
+]
+
+
+buyItem(itemsToBuy)
