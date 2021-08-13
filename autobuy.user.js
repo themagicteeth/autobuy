b56f8634aaf33cec5f97984355922bf468dd0ddf
Billy⚡️ <will.js@protonmail.com>
2021-08-13 17:49:58 -0500

    Update autobuy.user.js

--- autobuy.user.js
+++ autobuy.user.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name        Auto Buy - Core
-// @namespace   Neopets Auto Buy
-// @match       none
-// @grant       none
-// @version     1.0
-// @author      -
-// @description Core functionality for the auto buying scripts
-// ==/UserScript==
-
-
 function getItemsInStockToBuy() {
     const itemsInStock = []
     document.querySelectorAll('.item-img').forEach(item => itemsInStock.push(item.getAttribute('data-name')));
@@ -22,4 +11,3 @@ function buyItem() {
 }
 
 buyItem()
-  
