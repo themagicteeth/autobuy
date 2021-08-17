772b0b5ce5363a817b88d37f925da0a352525205
Billy⚡️ <will.js@protonmail.com>
2021-08-17 15:49:01 -0500

    Update 2.0.js

--- autobuy2/2.0.js
+++ autobuy2/2.0.js
@@ -12,15 +12,7 @@
 // @description Autobuyer for Neopets shops
 // ==/UserScript==
 
-// If freezing after purchase 2,000 to 4,000 is ok
-const FROM = 8000
-const TO = 16000
 
-const refreshTimes = Math.floor(Math.random() * parseFloat(TO - FROM)) + parseFloat(FROM);
-
-setTimeout(function() {
-  location.href = document.location
-}, refreshTimes)
 
 
 function backToShop() {
@@ -37,9 +29,17 @@ function getStock() {
 function checkAutoBuy(itemsToBuy) {
   const inStock = getStock()
   if (inStock !== []) {
-    const toBuy = itemsToBuy.filter(item => itemsInStock.includes(item));
+    const toBuy = itemsToBuy.filter(item => inStock.includes(item));
     if (toBuy.length === 0) {
+      // If freezing after purchase 2,000 to 4,000 is ok
+      const FROM = 8000
+      const TO = 16000
       
+      const refreshTimes = Math.floor(Math.random() * parseFloat(TO - FROM)) + parseFloat(FROM)
+      
+      setTimeout(function() {
+        location.href = document.location
+      }, refreshTimes)
     } else {
         document.querySelector(`.item-img[data-name='${toBuy[0]}']`).click()
         document.getElementById('confirm-link').click()
@@ -48,30 +48,87 @@ function checkAutoBuy(itemsToBuy) {
   }
 }
 
-function smartHaggle() {
-    const hagglePrice = (
-      document.getElementById('shopkeeper_makes_deal')
-      .find('b')
-      .get(0)
-      .innerHTML
-      .match("([0-9-,]+)")[0])
-      .replace(",", "")
+function solveCaptcha(url) {
+  return new Promise(resolve => {
+    let captcha = new Image();
+    captcha.src = url;
+    captcha.onload = () => {
+      const width = captcha.width
+      const height = captcha.height
 
-    const val = [
-      hagglePrice.substr(0, 1),
-      hagglePrice.substr(1, 1)
-    ]
+      let canvas = unsafeWindow.document.createElement('canvas');
+      canvas.width = width;
+      canvas.height = width;
+      canvas.getContext("2d").drawImage(captcha, 0, 0);
 
-    let endPrice = ""
-    for(let x = 0; x < hagglePrice.length; x++) {
-        endPrice += val[(x % 2)]
+      const imgData = canvas.getContext("2d").getImageData(0, 0, width, height)
+      let lowy = 999
+      let lowx = 999
+      let low = 999
+
+      for (let x = 0; x < imgData.width; x++) {
+        for (let y = 0; y < imgData.height; y++) {
+          const i = x * 4 + y * 4 * imgData.width
+          const avg = Math.floor((imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3);
+          if (avg < low) {
+            low = avg
+            lowx = x
+            lowy = y
+          }
+        }
+      }
+      resolve({lowx, lowy})
     }
-  
-    return endPrice
+  })
+}
+
+function smartHaggle() {
+  const hagglePrice = (
+    document.getElementById('shopkeeper_makes_deal')
+    .find('b')
+    .get(0)
+    .innerHTML
+    .match("([0-9-,]+)")[0])
+    .replace(",", "")
+
+  const val = [
+    hagglePrice.substr(0, 1),
+    hagglePrice.substr(1, 1)
+  ]
+
+  let endPrice = ""
+  for(let x = 0; x < hagglePrice.length; x++) {
+      endPrice += val[(x % 2)]
+  }
+
+  return endPrice
 }
 
 
-function haggleForItem() {
+async function haggleForItem() {
   const hagglePrice = smartHaggle()
-  document.querySelector('input[name=current_offer]').value = hagglePrice;
+  document.querySelector('input[name=current_offer]').value = hagglePrice
+  
+  const {lowx: x, lowy: y} = await solveCaptcha(document.querySelector('input[type="image"]').src)
+  
+  setTimeout(()=> {
+    let haggleform = document.querySelector('form[name="haggleform"]')
+    let newInput = document.createElement("input")
+    let newInput2 = document.createElement("input")
+
+    newInput.type = "hidden"
+    newInput.name = "x"
+    newInput.value = x
+    haggleform.appendChild(newInput)
+
+    newInput2.type = "hidden"
+    newInput2.name = "y"
+    newInput2.value = y
+    haggleform.appendChild(newInput2)
+    haggleform.submit()
+  }, (min, max) => { return Math.floor(Math.random() * (max - min + 1)) + min })
+}
+
+function autoBuy(itemsToBuy) {
+  
 }
